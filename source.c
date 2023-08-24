6e729e647c0118106aef0473d4dd5e50f9dfad56
Ensar Aydin Kurubacak <eaydin.kurubacak@gmail.com>
2023-08-24 17:08:22 +0300

    calculateTotalAndShowBasket() function takes reference of basket struct and then calculates total from scratch before printing what inside of basket. checkOutAndPrintReceipt function prints receipt after check out and manipulate stock amount of products in basket after it. It uses real time and date information at the end of it.

--- source.c
+++ source.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <time.h>
 
 struct User // customer
 {
+    int user_id;
     char user_name[25];
     char password[25];
     int login_right; // default : login_right = 3 , unblocked işleminde login_right = 3 , login_right == 0 -> blocked , login_right != 0 -> not blocked
@@ -20,12 +22,12 @@ int login(char user_name[], char password[], struct User users[], int number_of_
             if (!strcmp(password, "qwerty"))
             {
                 printf("Successfully logged in!\n");
-                return 1; // admin found
+                return 100; // admin found
             }
             else
             {
                 printf("Your user name and/or password is not correct. Please try again!\n");
-                return 0; // admin found, wrong password
+                return -1; // admin found, wrong password
             }
         }
         else
@@ -35,7 +37,7 @@ int login(char user_name[], char password[], struct User users[], int number_of_
                 if (users[i].login_right == 0)
                 {
                     printf("Your account has been blocked. please contact the administrator.\n");
-                    return 0; // customer blocked
+                    return -1; // customer blocked
                 }
                 else
                 {
@@ -43,7 +45,7 @@ int login(char user_name[], char password[], struct User users[], int number_of_
                     {
                         printf("Successfully logged in!\n");
                         users[i].login_right = 3;
-                        return 2; // customer found
+                        return users[i].user_id; // customer found
                     }
                     else
                     {
@@ -52,30 +54,32 @@ int login(char user_name[], char password[], struct User users[], int number_of_
                         {
                             printf("Your user name and/or password is not correct.\n");
                             printf("Your account has been blocked. please contact the administrator.\n");
-                            return 0; // customer blocked
+                            return -1; // customer blocked
                         }
                         else
                         {
                             printf("Your user name and/or password is not correct. Please try again!\n");
                         }
-                        return 0; // customer found, wrong password
+                        return -1; // customer found, wrong password
                     }
                 }
             }
         }
     }
     printf("Please enter valid admin or customer username.\n");
-    return 0; // not found admin or customer
+    return -1; // not found admin or customer
 }
 
 void createInitialUsers(struct User users[])
 {
     // ahmet created
+    users[0].user_id = 0;
     strcpy(users[0].user_name, "ahmet");
     strcpy(users[0].password, "1234");
     users[0].login_right = 3;
 
     // zeynep created
+    users[1].user_id = 1;
     strcpy(users[1].user_name, "zeynep");
     strcpy(users[1].password, "4444");
     users[1].login_right = 3;
@@ -157,6 +161,97 @@ void searchForAProduct(char search[], struct Product products[], struct Product 
     }
 }
 
+struct Basket
+{
+    struct Product products[13];
+    int size_basket;
+    int product_amount[13];
+    int total;
+};
+
+void createInitialBaskets(struct Basket baskets[])
+{
+    for (int i = 0; i < 100; i++)
+    {
+        baskets[i].size_basket = 0;
+        baskets[i].total = 0;
+    }
+}
+
+void calculateTotalAndShowBasket(struct Basket *basket)
+{
+    basket->total = 0;
+    for (int i = 0; i < basket->size_basket; i++)
+    {
+        printf("%d.%s price=%d amount=%d total=%d$\n", i + 1, basket->products[i].name, basket->products[i].price, basket->product_amount[i], basket->product_amount[i] * basket->products[i].price);
+        basket->total += basket->product_amount[i] * basket->products[i].price;
+    }
+    printf("Total %d$\n\n", basket->total);
+}
+
+void removeItemEditBasket(struct Basket *basket, int remove_item_index)
+{
+    for (int i = 0; i < basket->size_basket; i++)
+    {
+        if (i > remove_item_index)
+        {
+            basket->products[i - 1] = basket->products[i];
+            basket->product_amount[i - 1] = basket->product_amount[i];
+        }
+        else
+        {
+            basket->total -= basket->products[i].price * basket->product_amount[i];
+        }
+    }
+    basket->size_basket--;
+}
+
+void checkOutAndPrintReceipt(struct Product products[], struct Basket *basket)
+{
+    time_t current_time;
+    struct tm *time_info;
+    char time_string[80];
+    if (basket->size_basket == 0)
+    {
+        printf("user's basket is empty.\n");
+    }
+    else
+    {
+        for (int i = 0; i < basket->size_basket; i++)
+        {
+            for (int j = 0; j < 13; j++)
+            {
+                if (!strcmp(basket->products[i].name, products[j].name))
+                {
+                    products[j].stock_amount -= basket->product_amount[i];
+                }
+            }
+        }
+
+        printf("Processing your receipt...\n");
+        printf("******* Medipol Online Market ********\n");
+        printf("**************************************\n");
+        printf("444 8 544\n");
+        printf("medipol.edu.tr\n");
+        printf("_ _ _ _ _ _ _ _ _ _ _ _\n");
+        for (int i = 0; i < basket->size_basket; i++)
+        {
+            printf("%s %d$ amount=%d total=%d$\n", basket->products[i].name, basket->products[i].price, basket->product_amount[i], basket->products[i].price * basket->product_amount[i]);
+        }
+        printf("_ _ _ _ _ _ _ _ _ _ _ _\n");
+        printf("Total %d$", basket->total);
+        printf("_ _ _ _ _ _ _ _ _ _ _ _\n");
+        time(&current_time);
+        time_info = localtime(&current_time);
+        strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", time_info);
+        printf("%s\n", time_string);
+        printf("Thank You for using our Market!\n");
+
+        basket->size_basket = 0;
+        basket->total = 0;
+    }
+}
+
 int main()
 {
     struct User users[100];
@@ -164,7 +259,7 @@ int main()
     int number_of_users = 2;
     char user_name[25];
     char password[25];
-    int login_status = 0; // default : login_status = 0 , login_status == 1,2 -> login successful
+    int login_id = -1; // default : login_id = -1 , login_id == 1,2,.....,99 -> login successful (customer id), login_id == 100 -> login successful (admin)
     int main_menu_choice = 0;
     char search[25];
     struct Product search_result[13];
@@ -172,9 +267,14 @@ int main()
     int product_choice = 0;
     int product_amount = 0;
     bool is_return_mm;
+    struct Basket baskets[100];
+    int basket_sub_menu_choice = 0;
+    int temp_bsm_choice = 0; // temp basket sub menu choice
+    bool is_return_bsm;
 
     createInitialUsers(users);
     createProducts(products);
+    createInitialBaskets(baskets);
 
     printf("****Welcome to Medipol Online Market****\n");
     printf("Please log in by providing your user credentials:\n");
@@ -184,13 +284,13 @@ int main()
         scanf("%s", user_name);
         printf("Password:");
         scanf("%s", password);
-        login_status = login(user_name, password, users, number_of_users);
+        login_id = login(user_name, password, users, number_of_users);
 
-    } while (!login_status);
+    } while (login_id == -1);
 
     printf("Welcome, %s! Please choose one of the following options by entering the corresponding menu number.\n", user_name);
 
-    if (login_status == 1)
+    if (login_id == 100)
     {
     }
     else
@@ -271,12 +371,92 @@ int main()
                         }
                         else
                         {
+                            baskets[login_id].products[baskets[login_id].size_basket] = search_result[product_choice - 1];
+                            baskets[login_id].product_amount[baskets[login_id].size_basket] = product_amount;
+                            baskets[login_id].size_basket++;
+
                             printf("Added %s into your Basket.\n", search_result[product_choice - 1].name);
                             is_return_mm = true;
                             printf("Going back to main menu...\n");
                         }
                     }
                 }
+            }
+            else if (main_menu_choice == 2)
+            {
+                is_return_mm = false;
+                is_return_bsm = false;
+                do
+                {
+                    if (baskets[login_id].size_basket == 0)
+                    {
+                        printf("user's basket is empty.\n");
+                    }
+                    else
+                    {
+                        printf("Your basket contains:\n");
+                        calculateTotalAndShowBasket(&baskets[login_id]);
+                    }
+                    printf("Please choose an option:\n");
+                    printf("1.Update amount\n");
+                    printf("2.Remove an item\n");
+                    printf("3.Check out\n");
+                    printf("4.Go back to main menu\n");
+                    printf("Your selection: ");
+                    scanf("%d", &basket_sub_menu_choice);
+
+                    if (basket_sub_menu_choice == 1)
+                    {
+                        printf("Please select which item to change its amount : ");
+                        scanf("%d", &temp_bsm_choice);
+                        printf("Please type the new amount : ");
+                        scanf("%d", &product_amount);
+
+                        if (product_amount > baskets[login_id].products[temp_bsm_choice - 1].stock_amount)
+                        {
+                            do
+                            {
+                                printf("Sorry! The amount exceeds the limit, Please try again with smaller amount\n");
+                                printf("Amount (Enter 0 for main menu): ");
+                                scanf("%d", &product_amount);
+                                is_return_mm = product_amount == 0 ? true : false;
+                            } while (!is_return_mm && product_amount > baskets[login_id].products[temp_bsm_choice - 1].stock_amount);
+                        }
+                        else
+                        {
+                            baskets[login_id].product_amount[temp_bsm_choice - 1] = product_amount;
+                            printf("Your basket now contains:\n");
+                            calculateTotalAndShowBasket(&baskets[login_id]);
+                        }
+
+                        if (is_return_mm)
+                        {
+                            printf("Going back to main menu...\n");
+                        }
+                        else
+                        {
+                            is_return_bsm = true;
+                        }
+                    }
+                    else if (basket_sub_menu_choice == 2)
+                    {
+                        printf("Please select which item to remove : ");
+                        scanf("%d", &temp_bsm_choice);
+                        removeItemEditBasket(&baskets[login_id], temp_bsm_choice - 1);
+                        if (baskets[login_id].size_basket != 0)
+                        {
+                            printf("Your basket contains:\n");
+                            calculateTotalAndShowBasket(&baskets[login_id]);
+                        }
+
+                        is_return_bsm = true;
+                    }
+                    else if (basket_sub_menu_choice == 3)
+                    {
+                        checkOutAndPrintReceipt(products, &baskets[login_id]);
+                    }
+
+                } while (is_return_bsm);
             }
         } while (is_return_mm);
     }
